d19d6ec40fbb755d385298e2653325372a84c05f
Arun Shankar Tiwari <arunstiwari@gmail.com>
2020-01-05 00:10:39 +0530

    Refactored the code to implement styled component in Header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,15 +1,30 @@
 import React from 'react';
-import './Header.css';
-import Link from "../link/Link";
+import styled from 'styled-components';
+
+const HeaderWrapper = styled.header`
+  background-color: #282c34;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const Logo = styled.img`
+  height: 64px;
+  pointer-events: none;
+`;
 
 const Header = ({logo}) => {
   return (
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
+    <HeaderWrapper>
+      <Logo src={logo}  alt="logo" />
       <h1>
         My Github Portfolio
       </h1>
-    </header>
+    </HeaderWrapper>
   );
 };
 
